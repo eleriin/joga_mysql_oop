4f0d9c6185eb906e52d4036ee51520dc9496985a
Eleriin <eleriin.kurs@voco.ee>
2026-02-18 11:52:38 +0200

    Fix existing user and password limitation errors

--- controllers/user.js
+++ controllers/user.js
@@ -4,24 +4,45 @@ const userModel = new userDbModel()
 
 class userControllerClass {
     async register(req,res){
-        const cryptPassword = await bcrypt.hash(req.body.password, 10)
+        try{
+            const { username, email, password } = req.body
+
+            const existingUser = await userModel.findByEmail(email)
+            if (existingUser){
+                return res.status(400).json({message:'Email is already registered'})
+            }
+            if(password.length < 6){
+                return res.status(400).json({message: 'Password must be at least 6 charaters long'})
+            }
+
+            const passwordRegex = /^(?=.*[A-Z])(?=.*\d).+$/;
+            if (!passwordRegex.test(password)) {
+                return res.status(400).json({ message: 'Password must contain at least one uppercase letter and one number' });
+            }
+            
+            const cryptPassword = await bcrypt.hash(password, 10)
             const registeredId = await userModel.create({
-                username: req.body.username,
-                email: req.body.email, 
+                username, 
+                email,
                 password: cryptPassword
             })
-            if(registeredId){
-                const userData = await userModel.findById(registeredId)
+
+        
+            if (registeredId){
+                const userData = await userModel.findUserById(registeredId)
                 req.session.user = {
-                    username: userData.username,
+                    username: userData.username, 
                     user_id: userData.id
                 }
-                res.json({
+                return res.status(201).json({
                     message: 'New user is registered',
                     user_session: req.session.user
                 })
             }
+        } catch (error){
+            res.status(500).json({message: error.message})
+        }
     }
+}
 
-
-}module.exports = userControllerClass
+module.exports = userControllerClass
